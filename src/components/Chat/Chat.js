e10b32576a5b0397c2fc0a5e1c27c9ed5ff1cc51
Clément Vallat <clement.vallat@yahoo.com>
2021-03-08 11:35:03 +0100

    add chat feature

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import spotifyService from './../../services/spotify-service';
+import ChatRoom from './ChatRoom';
 
  class Chat extends Component {
-
     state = {
         name: "",
         picture: "",
@@ -24,11 +24,14 @@ import spotifyService from './../../services/spotify-service';
         this.retrieveArtist();
     }
 
+
+
+
     render() {
         return (
             <div>
                 <h1>Chat with other fans of {this.state.name}</h1>
-            
+                <ChatRoom username={this.state.name}/>
                 <button className="back-btn" onClick={this.props.history.goBack}>
                    Go Back
                 </button>
---
+++ src/components/Chat/ChatRoom.js
@@ -0,0 +1,103 @@
+import io from "socket.io-client";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { withAuth } from "./../../context/auth-context";
+
+
+const username = "you";
+
+const socket = io("http://localhost:5000", {
+  transports: ["websocket", "polling"]
+});
+
+const ChatRoom = ({}) => {
+  const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      socket.emit("username", username);
+    });
+
+    socket.on("users", users => {
+      setUsers(users);
+    });
+
+    socket.on("message", message => {
+      setMessages(messages => [...messages, message]);
+    });
+
+    socket.on("connected", user => {
+      setUsers(users => [...users, user]);
+    });
+
+    socket.on("disconnected", id => {
+      setUsers(users => {
+        return users.filter(user => user.id !== id);
+      });
+    });
+  }, []);
+
+  const submit = event => {
+    event.preventDefault();
+    socket.emit("send", message);
+    setMessage("");
+  };
+  
+
+  return (
+    <div className="chat-container">
+      <div className="row">
+        <div className="col-md-12 mt-4 mb-4">
+          <h6>Hello {username}</h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-8">
+          <h6>Messages</h6>
+          <div id="messages">
+            {messages.map(({ username, date, text }, index) => (
+              <div key={index} className="message-display">
+                <div className="col-md-3">
+                  {moment(date).format("h:mm")}
+                </div>
+                <div className="col-md-2">{username}</div>
+                <div className="col-md-2">{text}</div>
+              </div>
+            ))}
+          </div>
+          <form onSubmit={submit} id="form">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                onChange={e => setMessage(e.currentTarget.value)}
+                value={message}
+                id="text"
+              />
+              <span className="input-group-btn">
+                <button id="submit" type="submit" className="btn btn-primary">
+                  Send
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+        <div className="col-md-4">
+          <h6>Users</h6>
+          <ul id="users">
+            {users.map(({ name, id }) => (
+              <li key={id}>{name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withAuth(ChatRoom);
